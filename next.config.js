4f98560b8ee36851cdcec6c0bece79fda66490ae
Simana Poudel <poudelsimana22@gmail.com>
2023-09-26 13:42:49 +0545

    next config.js changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    images: true, // Enable the experimental images feature
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
